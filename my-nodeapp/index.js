b24b8e36874780c3fc0e886ae9faf2fb8720f037
diyor0929 <diyorsultonov0727@gmai.com>
2025-12-11 15:48:04 +0500

    add sample node.js app

---
+++ my-nodeapp/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve static files (index.html)
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/api", (req, res) => {
+  res.json({
+    message: "API endpoint working!",
+    status: "success",
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
