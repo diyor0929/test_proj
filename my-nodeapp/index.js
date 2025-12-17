52eb17c974fc2496516389c19365cd5829259c68
diyor0929 <diyorsultonov092903@gmail.com>
2025-12-17 11:56:59 +0500

    Update index.js

--- my-nodeapp/index.js
+++ my-nodeapp/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+print="helo"
 
 // Serve static files (index.html)
 app.use(express.static(path.join(__dirname, "public")));
